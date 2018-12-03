9cd2a6959e12d364682a16894444649cf8c6dd78
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-04 00:25:43 +0600

    seed settings table

---
+++ database/seeds/SettingsTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Setting;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SettingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('settings')->delete();
+
+        Setting::create([
+            'site_name' => 'Mily POS',
+            'site_name_abbr' => 'MPOS',
+            'default_currency' => 'USD'
+        ]);
+    }
+}
