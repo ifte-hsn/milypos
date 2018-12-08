79a582e9d51f94776e6f0ec3190b8fec17d704f9
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-08 10:19:54 +0600

    fixed extra " in body tag

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -85,7 +85,7 @@
     <link rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition skin-blue sidebar-mini {{ (session('menu_state')!='open') ? 'sidebar-mini sidebar-collapse' : ''  }}"">
+<body class="hold-transition skin-blue sidebar-mini {{ (session('menu_state')!='open') ? 'sidebar-mini sidebar-collapse' : ''  }}">
 <div class="wrapper">
 
 @include('layouts.partials.header')
