a43e80e4a21082919ce052400bae9b6fb106f736
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-09 15:04:58 +0600

    added userstransformar for formatting the data

---
+++ app/Http/Transformers/UsersTransformer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Transformers;
+
+use App\Helpers\Helper;
+use App\User;
+use Illuminate\Database\Eloquent\Collection;
+
+class UsersTransformer
+{
+
+    public function transformUsers(Collection $users, $total)
+    {
+        $array = array();
+        foreach ($users as $user) {
+            $array[] = self::transformUser($user);
+        }
+
+        return (new DatatablesTransformer)->transformDatatables($array, $total);
+    }
+
+    public function transformUser(User $user)
+    {
+        $array = [
+            'id' => (int) $user->id,
+            'first_name' => e($user->first_name),
+            'last_name' => e($user->last_name),
+            'phone' => ($user->phone) ? e($user->phone) : null,
+            'address' => ($user->address) ? e($user->address) : null,
+            'city' => ($user->city) ? e($user->city) : null,
+            'state' => ($user->state) ? e($user->state) : null,
+            'country' => ($user->country) ? e($user->country) : null,
+            'zip' => ($user->zip) ? e($user->zip) : null,
+            'email' => ($user->email) ? e($user->email) : null,
+            'activated' => ($user->activated) ? e($user->activated) : null,
+            'created_at' => Helper::getFormattedDateObject($user->created_at, 'datetime'),
+            'updated_at' => Helper::getFormattedDateObject($user->updated_at, 'datetime'),
+            'last_login' => Helper::getFormattedDateObject($user->last_login, 'datetime')
+        ];
+        return $array;
+    }
+
+    public function transformUsersDatatable($users) {
+        return (new DatatablesTransformer)->transformDatatables($users);
+    }
+}
