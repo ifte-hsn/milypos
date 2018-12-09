84099f464578c5427ed5be04b8304726e90f8a2e
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-09 15:04:12 +0600

    added helper added getFormattedDateObject for getting formatted date

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,35 @@
+<?php
+
+
+namespace App\Helpers;
+
+
+use App\Setting;
+use Carbon\Carbon;
+
+class Helper
+{
+    public static function getFormattedDateObject($date, $type="datetime", $array = true)
+    {
+        if($date == '') {
+            return null;
+        }
+
+        $settings = Setting::getSettings();
+        $tempDate = new Carbon($date);
+
+        if($type == 'datetime') {
+            $dt['datetime'] = $tempDate->format('Y-m-d H:i:s');
+            $dt['formatted'] = $tempDate->format($settings->date_display_format);
+        } else {
+            $dt['date'] = $tempDate->format('Y-m-d');
+            $dt['formatted'] = $tempDate->format($settings->date_display_format);
+        }
+
+        if($array = true) {
+            return $dt;
+        }
+
+        return $dt['formatted'];
+    }
+}
