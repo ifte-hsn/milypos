7fc4a10c90dc199a0e4776ee325676990b3e3d8f
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-10 15:09:19 +0600

    added api response formatter

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -32,4 +32,30 @@ class Helper
 
         return $dt['formatted'];
     }
+
+    /**
+     * Format standard API Response
+     *
+     * @param $status Status of the operaion
+     * @param null $payload
+     * @param null $messages
+     * @return mixed
+     */
+    function formatStandardApiResponse($status, $payload = null, $messages = null)
+    {
+        $array['status'] = $status;
+        $array['messages'] = $messages;
+
+        if (($messages) && (is_array($messages)) && (count($messages) > 0)) {
+            $array['messages'] = $messages;
+        }
+
+        if (($payload)) {
+            $array['payload'] = $payload;
+        } else {
+            $array['payload'] = null;
+        }
+
+        return $array;
+    }
 }
