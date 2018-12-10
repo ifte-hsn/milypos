7522463095f30e8694c2a08ae110a5e5e1017b79
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-11 00:35:50 +0600

    removed some unecessry fields form settings table

--- database/migrations/2018_12_03_170503_create_settings_table.php
+++ database/migrations/2018_12_03_170503_create_settings_table.php
@@ -18,28 +18,21 @@ class CreateSettingsTable extends Migration
             $table->integer('user_id')->nullable();
             $table->integer('per_page')->default(20);
             $table->string('site_name', 100)->default('Mily POS');
-            $table->string('site_name_abbr', 100)->default('M');
+            $table->string('company_name', 100)->default('Mily POS');
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('fax')->nullable();
+            $table->string('website')->nullable();
+            $table->string('address')->nullable();
+            $table->string('vat_no')->nullable();
+            $table->string('bank_account_no')->nullable();
             $table->integer('qr_code')->nullable()->default(NULL);
-            $table->string('qr_text', 32)->nullable()->default(NULL);
-            $table->integer('display_asset_name')->nullable()->default(NULL);
-            $table->integer('display_checkout_date')->nullable()->default(NULL); // TODO: Check and remove if necessary
-            $table->integer('display_eol')->nullable()->default(NULL); // TODO: Check and remove if necessary
-            $table->integer('auto_increment_assets')->default(0);
-            $table->string('auto_increment_prefix')->default(0);
-            $table->boolean('load_remote')->default(1); // TODO: Check and remove if necessary
-            $table->string('logo')->nullable()->default(NULL);
-            $table->string('mini_logo')->nullable()->default(NULL);
+            $table->string('qr_text', 32)->nullable()->default(NULL);   $table->string('logo')->nullable()->default(NULL);
             $table->integer('thumbnail_max_h')->nullable()->default('50');
             $table->string('header_color')->nullable()->default(NULL);
-            $table->string('alert_email')->nullable()->default(NULL);
-            $table->boolean('alerts_enabled')->default(1);
-            $table->longText('default_eula_text')->nullable()->default(NULL); // TODO: Check and remove if necessary
             $table->string('barcode_type')->nullable()->default('QRCODE');
             $table->string('alt_barcode')->nullable()->default('C128');
             $table->boolean('alt_barcode_enabled')->nullable()->default('1');
-            $table->string('slack_endpoint')->nullable()->default(NULL);
-            $table->string('slack_channel')->nullable()->default(NULL);
-            $table->string('slack_botname')->nullable()->default(NULL);
             $table->string('default_currency',10)->nullable()->default(NULL);
             $table->text('custom_css')->nullable()->default(NULL);
             $table->enum('brand',['text_only', 'logo_only', 'text_logo'])->default('text_only');
