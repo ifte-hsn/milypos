541b4d74a3bd1fa572b844b4bf442f8239018f25
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-20 00:54:40 +0600

    added javascript files in wappack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,15 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.scripts([
+    'node_modules/jquery/dist/jquery.min.js',
+    'resources/js/jquery-ui.min.js',
+    'resources/js/bootstrap.min.js',
+    'resources/js/select2.full.min.js',
+    'resources/js/ekko-lightbox.min.js',
+    'resources/js/adminlte.js',
+    'resources/js/demo.js',
+    'resources/js/script.js',
+    'node_modules/jquery-slimscroll/jquery.slimscroll.js',
+], 'public/js/all.js')
    .sass('resources/sass/app.scss', 'public/css/all.css');
