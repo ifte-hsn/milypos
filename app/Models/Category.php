2d6cdc03592ea9c71bdbf9995fb1f57e285bf0d5
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-25 10:28:41 +0600

    added fillable attributes

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use ValidatingTrait;
+
+    protected $rules = [
+      'name' => 'required'
+    ];
+
+    protected $fillable = ['name'];
+    protected $dates = ['deleted_at'];
+
+    public function scopeGetDeleted($query)
+    {
+        return $query->withTrashed()->whereNotNull('deleted_at');
+    }
 }
