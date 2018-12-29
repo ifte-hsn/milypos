7c77ad89bf03b082040855149cbc6081285b5845
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-29 23:13:09 +0600

    created Role Presenter

--- app/Presenters/RolePresenter.php
+++ app/Presenters/RolePresenter.php
@@ -5,5 +5,40 @@ namespace App\Presenters;
 
 class RolePresenter
 {
+    public static function dataTableLayout()
+    {
+        $layout = [
+            [
+                'field' => 'checkbox',
+                'checkbox' => 'true',
+            ],
+            [
+                'field' => 'id',
+                'searchable' => false,
+                'sortable' => true,
+                'switchable' => true,
+                'title' => __('general.id'),
+                'visible' => false
+            ],
+            [
+                'field' => 'name',
+                'searchable' => true,
+                'sortable' => true,
+                'title' => __('general.name'),
+                'visible' => true,
+                'formatter' => 'rolesLinkFormatter'
+            ],
+            [
+                'field' => 'actions',
+                'searchable' => false,
+                'sortable' => false,
+                'switchable' => false,
+                'title' => __('general.actions'),
+                'visible' => true,
+                'formatter' => 'rolesActionsFormatter'
+            ]
+        ];
 
+        return json_encode($layout);
+    }
 }
