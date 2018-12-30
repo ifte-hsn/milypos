27cb3deea4ee731196eb46ff9bb49d85ca0f89a6
Iftekhar Hossain <ifte.hsn@gmail.com>
2018-12-30 12:28:00 +0600

    added permission input for users module

--- resources/views/roles/edit.blade.php
+++ resources/views/roles/edit.blade.php
@@ -43,6 +43,90 @@
                         </div><!-- .col-md-6 col-sm-6 col-xs-12 -->
                     </div><!-- form-group -->
 
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover table-striped reports-table">
+
+                            <thead>
+                                <tr>
+                                    <th colspan="6"
+                                        class="text-center"> {{ ($role->name) ? $role->name: '' }} {{ __('general.permissions') }}</th>
+                                </tr>
+
+                                <tr>
+                                    <th rowspan="2" class="text-center">{{ __('general.module_name') }}</th>
+                                    <th colspan="5" class="text-center">{{ __('general.permissions') }}</th>
+                                </tr>
+
+                                <tr>
+                                    <th class="text-center">{{ __('general.view') }}</th>
+                                    <th class="text-center">{{ __('general.add') }}</th>
+                                    <th class="text-center">{{ __('general.edit') }}</th>
+                                    <th class="text-center">{{ __('general.delete') }}</th>
+                                    <th class="text-center">{{ __('general.miscellaneous') }}</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <strong>{{ __('general.users') }}</strong>
+                                    </td>
+
+                                    <td class="text-center">
+                                        <div class="checkbox icheck">
+                                            <label>
+                                                <input name="Read User" id="read_user" type="checkbox">
+                                            </label>
+                                        </div>
+                                    </td><!-- read -->
+
+                                    <td class="text-center">
+                                        <div class="checkbox icheck">
+                                            <label>
+                                                <input name="Create User" id="create_user" type="checkbox">
+                                            </label>
+                                        </div>
+                                    </td><!-- Create -->
+
+                                    <td class="text-center">
+                                        <div class="checkbox icheck">
+                                            <label>
+                                                <input name="Update User" id="update_user" type="checkbox">
+                                            </label>
+                                        </div>
+                                    </td><!-- Update -->
+
+
+                                    <td class="text-center">
+                                        <div class="checkbox icheck">
+                                            <label>
+                                                <input name="Delete User" id="delete_user" type="checkbox">
+                                            </label>
+                                        </div>
+                                    </td><!-- Update -->
+
+                                    <td>
+                                        <span style="display:inline-block;">
+                                            <div class="checkbox icheck">
+                                                <label for="export_user" class="padding05">
+                                                    <input name="Export Users" id="export_user" type="checkbox"> <strong>{{ __('general.export') }}</strong>
+                                                </label>
+                                            </div>
+                                        </span>
+
+                                        <span style="display:inline-block;">
+                                            <div class="checkbox icheck">
+                                                <label for="bulk_delete" class="padding05">
+                                                    <input name="Bulk Delete" id="bulk_delete" type="checkbox"> <strong>{{ __('general.bulk_checkin_and_delete') }}</strong>
+                                                </label>
+                                            </div>
+                                        </span>
+                                    </td><!-- Update -->
+                                </tr> <!-- user module -->
+                            </tbody>
+                        </table>
+                    </div><!-- table-responsive -->
+
                 </div><!-- .box-body -->
                 <div class="box-footer">
                     <div class="row">
@@ -57,3 +141,14 @@
     </div><!-- box-primary -->
 @endsection
 
+@section('page_scripts')
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' /* optional */
+            });
+        });
+    </script>
+@endsection
