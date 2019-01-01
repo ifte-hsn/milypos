c0557edd4da53b6572e3f9805b688b77d13f2683
ifte.hsn <ifte.hsn@gmail.com>
2019-01-01 15:14:56 +0600

    added category factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,12 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Shishir
- * Date: 1/1/2019
- * Time: 12:07 PM
- */
 
-class ProductFactory
-{
+use Faker\Generator as Faker;
 
-}
+$factory->define(App\Models\Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'code' => $faker->numberBetween(1000, 9999),
+        'stock' => $faker->numberBetween(1, 1000),
+        'description' => $faker->paragraph,
+        'purchase_price' => $faker->randomFloat(2, 1, 100),
+        'sell_price' => $faker->randomFloat(2, 2, 100),
+        'sales' => $faker->numberBetween(1, 1000),
+        'category_id' => $faker->numberBetween(1, 20)
+    ];
+});
