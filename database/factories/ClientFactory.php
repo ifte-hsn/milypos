c8a389aa327229797e889c30b8ffc703a8390785
Iftekhar Hossain <ifte.hsn@gmail.com>
2019-02-02 22:03:16 +0600

    added client factory

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Client;
+use Faker\Generator as Faker;
+
+$factory->define(Client::class, function (Faker $faker) {
+    return [
+        'email' => $faker->unique()->safeEmail,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'phone' => $faker->phoneNumber(),
+        'address' => $faker->address,
+        'city' => $faker->city,
+        'state' =>  $faker->state(),
+        'zip' => $faker->postcode,
+        'shopping' => $faker->randomNumber(),
+        'last_purchase' => $faker->dateTime($max = 'now', $timezone = null),
+        'dob' => $faker->dateTime($max = 'now', $timezone = null),
+        'sex' => 'Female'
+    ];
+});
