6682d6e7f1c640cdc3707a45e6717c871619c905
Iftekhar Hossain <ifte.hsn@gmail.com>
2019-02-02 19:51:36 +0600

    fixed a_user_can_login_with_correct_credential some times failed

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -2,13 +2,28 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\Setting;
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\BaseTest;
+use Tests\TestCase;
 
 
-class LoginTest extends BaseTest
+class LoginTest extends TestCase
 {
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        Setting::create([
+            'site_name' => 'Mily POS',
+            'logo' => 'logo.png',
+            'login_logo' => 'login_logo.png',
+            'favicon' => 'favicon.png',
+            'currency_id' => 1
+        ]);
+    }
 
     /** @test */
     public function user_can_view_a_login_form() {
@@ -27,12 +42,14 @@ class LoginTest extends BaseTest
 
     /** @test */
     public function a_user_can_login_with_correct_credential(){
-        $user = factory(User::class)->create();
+        $user = factory(User::class)->create([
+            'activated' => 1
+        ]);
         $response = $this->post('/login', [
             'email' => $user->email,
             'password' => 'secret'
-        ])->assertRedirect(route('home'));
-//        $response->assertStatus(302);
+        ]);
+        $response->assertStatus(302);
         $this->assertAuthenticatedAs($user);
     }
 
@@ -69,6 +86,14 @@ class LoginTest extends BaseTest
         $response->assertSessionHasErrors('email');
         $this->assertTrue(session()->hasOldInput('email'));
         $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    /** @test */
+    public function logout_authenticated_user(){
+        $user = factory(User::class)->create();
+        $this->actingAs($user)->post('/logout')
+            ->assertRedirect('/');
         $this->assertGuest();
     }
 }
