0fbc9c1fad315a71d60d0741661b9c4164d6ce37
Iftekhar Hossain <ifte.hsn@gmail.com>
2019-02-04 23:32:52 +0600

    added test for warehouse

---
+++ tests/Feature/WarehouseTest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Models\Warehouse;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Spatie\Permission\Models\Permission;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Faker\Factory as FakerFactory;
+
+class WarehouseTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    private $super_admin;
+    private $super_admin_role;
+    private $faker;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->super_admin = factory(User::class)->create(['activated' => 1]);
+
+        // Only super admin can access all the features
+        // so for the time being we will assign Super Admin
+        // Role to the user. To test other role and their
+        // permissions we have separate test.
+        $this->super_admin_role = Role::create(['name' => 'Super Admin']);
+        $this->super_admin->assignRole($this->super_admin_role);
+        $this->faker = FakerFactory::create();
+    }
+
+    /** @test */
+    public function it_has_an_index_page()
+    {
+        $this->actingAs($this->super_admin)->get(route('warehouses.index'))
+            ->assertViewIs('warehouses.index');
+    }
+
+    /** @test */
+    public function unauthorize_user_cannot_see_warehouse_index_page()
+    {
+
+        // create an unauthorized_user
+        $unauthorized_user = factory(User::class)->create(['activated' => true]);
+        $role = Role::create(['name' => 'Admin']);
+        $unauthorized_user->assignRole($role);
+
+        // create permission other than view_warehouse
+        $permission = Permission::create(['name'=>'view_user']);
+        $permission->assignRole($role);
+        $role->givePermissionTo($permission);
+
+
+
+        $this->actingAs($unauthorized_user)->get(route('warehouses.index'))
+            ->assertStatus(403);
+    }
+
+    /** @test */
+    public function user_with_view_warehouse_permission_can_see_index_page()
+    {
+        // create an unauthorized_user
+        $authorized_user = factory(User::class)->create(['activated' => true]);
+        $role = Role::create(['name' => 'Admin']);
+        $authorized_user->assignRole($role);
+
+        // create permission for viewing warehouse
+        $permission = Permission::create(['name'=>'view_warehouse']);
+        $permission->assignRole($role);
+        $role->givePermissionTo($permission);
+
+        $this->actingAs($authorized_user)->get(route('warehouses.index'))
+            ->assertViewIs('warehouses.index');
+    }
+
+
+    /** @test */
+    public function authenticated_user_with_view_warehouse_permission_can_see_warehouse_list()
+    {
+        // create an unauthorized_user
+        $authorized_user = factory(User::class)->create(['activated' => true]);
+        $role = Role::create(['name' => 'Admin']);
+        $authorized_user->assignRole($role);
+
+        // create permission for viewing warehouse
+        $permission = Permission::create(['name'=>'view_warehouse']);
+        $permission->assignRole($role);
+        $role->givePermissionTo($permission);
+
+        $warehouse = factory(Warehouse::class)->create();
+
+        $this->actingAs($authorized_user)->get(route('warehouses.list'))
+            ->assertJson([
+                'total' => '1',
+                "rows" => [
+                    [
+                        "id" => $warehouse->id,
+                        "code" => $warehouse->code,
+                        "name" => $warehouse->name,
+                        "phone" => $warehouse->phone,
+                        "email" => $warehouse->email,
+                        "address" => $warehouse->address,
+                    ]
+                ]
+            ]);
+    }
+}
