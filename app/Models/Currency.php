4339acb761f9a8926199b468d492a7cea86721f6
ifte.hsn <ifte.hsn@gmail.com>
2019-02-06 13:14:40 +0600

    added fillable element in courency

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -3,7 +3,27 @@
 namespace App\Models;
 
 
+use Watson\Validating\ValidatingTrait;
+
 class Currency extends MilyPosModel
 {
-    //
+    use ValidatingTrait;
+
+    protected $presenter = 'App\Presenters\CategoryPresenter';
+
+    protected $fillable = [
+        'country',
+        'currency',
+        'symbol',
+        'code',
+        'thousand_separator',
+        'decimal_separator',
+    ];
+
+    protected $rules = [
+        'country'              => 'required|string|unique|min:3',
+        'currency'              => 'required|string|min:1',
+        'code'                  => 'required|string|min:1',
+    ];
+
 }
