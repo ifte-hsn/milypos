18ad4c92659305c473a09104b7998ec65c2d9cbe
ifte.hsn <ifte.hsn@gmail.com>
2019-02-07 15:47:45 +0600

    remove permission seeding for admin role

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -22,40 +22,5 @@ class RolesTableSeeder extends Seeder
         foreach ($roles as $role) {
             Role::create(['name' => $role]);
         }
-
-
-        // Sync Permissions
-        $adminRole = Role::findByName('Admin');
-
-        // Create permission
-        $adminRole->syncPermissions([
-            // User Module
-            Permission::findByName('add_user'),
-            Permission::findByName('view_user'),
-            Permission::findByName('edit_user'),
-            Permission::findByName('delete_user'),
-            Permission::findByName('restore_user'),
-            Permission::findByName('bulk_delete_users'),
-            Permission::findByName('export_users'),
-
-            // Category
-            Permission::findByName('add_category'),
-            Permission::findByName('view_category'),
-            Permission::findByName('edit_category'),
-            Permission::findByName('delete_category'),
-            Permission::findByName('restore_category'),
-            Permission::findByName('export_categories'),
-            Permission::findByName('bulk_delete_categories'),
-
-            // Role
-            Permission::findByName('add_role'),
-            Permission::findByName('view_role'),
-            Permission::findByName('edit_role'),
-            Permission::findByName('delete_role'),
-            Permission::findByName('restore_role'),
-
-            // Settings
-            Permission::findByName('update_settings'),
-        ]);
     }
 }
