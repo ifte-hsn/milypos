7da37ec178270b97028854969cba535a6a1bdc0d
ifte.hsn <ifte.hsn@gmail.com>
2019-02-13 17:32:26 +0600

    added unique name for categories

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Category::class, function (Faker $faker) {
     return [
-      'name' => $faker->word
+      'name' => $faker->unique()->name
     ];
 });
 
